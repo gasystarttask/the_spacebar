6f4a89da993902af199cc6e2a6dc3b470b75bc8d
Rivo Hermann <rahajasonr@proximity.bbdo.fr>
2018-09-26 11:49:52 +0400

    add cache service

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,13 +3,14 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use \Symfony\Component\Routing\Annotation\Route;
 use \Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use \Symfony\Component\HttpKernel\Profiler\Profiler;
 use \Symfony\Component\HttpFoundation\JsonResponse;
 
 use Psr\Log\LoggerInterface;
 use Michelf\MarkdownInterface;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
 
 Class ArticleController extends AbstractController
 {
@@ -24,7 +25,7 @@ Class ArticleController extends AbstractController
   /**
    * @Route("/news/{slug}", name="article_show")
    */
-  public function show($slug, MarkdownInterface $markdown)
+  public function show($slug, MarkdownInterface $markdown, AdapterInterface $cache)
   {
     $comments = [
       'I ate a normal rock once. It did NOT taste like bacon!',
@@ -36,7 +37,7 @@ Class ArticleController extends AbstractController
 Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
 lorem proident [beef ribs](https://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
 labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
-turkey shank eu pork belly meatball non cupim.
+**turkey** shank eu pork belly meatball non cupim.
 Laboris beef ribs fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip. Pariatur
 laboris sunt venison, et laborum dolore minim non meatball. Shankle eu flank aliqua shoulder,
 capicola biltong frankfurter boudin cupim officia. Exercitation fugiat consectetur ham. Adipisicing
@@ -49,8 +50,13 @@ cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim ca
 fugiat.
 EOF;
 
-    $articleContent = $markdown->transform($articleContent);
+    $item = $cache->getItem('markdown_'.md5($articleContent));
+    if(!$item->isHit()) {
+      $item->set($markdown->transform($articleContent));
+      $cache->save($item);
+    }
 
+    $articleContent = $item->get();
     return $this->render('article/show.html.twig', [
       'title' => ucwords(str_replace('-', ' ', $slug)),
       'slug' => $slug,
